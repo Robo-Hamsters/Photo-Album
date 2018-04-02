0850c34482398271a037f28fef0782aaed91b83f
pavlina <pav_mits@hotmail.com>
2018-04-02 21:54:36 +0300

    Create Album Relation at database

--- ProjectPhotos/src/Model/Album.java
+++ ProjectPhotos/src/Model/Album.java
@@ -1,22 +1,29 @@
 package Model;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table (name="Albums")
 public class Album {
-    private String albumsDirectory="";
+    @Id
+    @Column (name="albumID")
+    private UUID  albumId;
+
+    @Column (name = "albumName")
     private String albumName="";
 
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="idPhoto",referencedColumnName = "idPhoto")
+     Photo photo;
 
 
 
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="userid",referencedColumnName = "userid")
+     User user;
 
 
-
-    public String getAlbumsDirectory() {
-        return albumsDirectory;
-    }
-
-    public void setAlbumsDirectory(String albumsDirectory) {
-        this.albumsDirectory = albumsDirectory;
-    }
 
     public String getAlbumName() {
         return albumName;
@@ -25,4 +32,5 @@ public class Album {
     public void setAlbumName(String albumName) {
         this.albumName = albumName;
     }
+
 }
--- ProjectPhotos/src/Model/Photo.java
+++ ProjectPhotos/src/Model/Photo.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class Photo {
 
     @Id
-    @Column(name="Id")
+    @Column(name="idPhoto")
     private UUID idPhoto;
     @Column (name="name")
     private String  namePhoto="";
