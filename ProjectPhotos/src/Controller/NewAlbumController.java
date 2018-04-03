fe786fe8186a529e20f2247d1569031ec4d3dc21
pavlina <pav_mits@hotmail.com>
2018-04-04 01:05:29 +0300

    edit newAlbumController

--- ProjectPhotos/src/Controller/NewAlbumController.java
+++ ProjectPhotos/src/Controller/NewAlbumController.java
@@ -4,8 +4,12 @@ import Model.Album;
 import Model.User;
 import Repo.AlbumRepo;
 import Repo.DBConnector;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.UUID;
 
@@ -17,7 +21,7 @@ public class NewAlbumController {
     private User user;
 
     @FXML
-    void createAlbum()
+    void createAlbum(ActionEvent event)
     {
         DBConnector con = new DBConnector();
         con.databaseConnect();
@@ -27,7 +31,28 @@ public class NewAlbumController {
         album.setAlbumName(textAlbumName.getText());
         album.setUser(user);
 
+        if(!textAlbumName.getText().isEmpty())
+        {
+
         albumRepo.dbInsertAlbum(album,con);
+
+        Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Tuxedo View");
+        alert.setContentText("You create an album");
+        alert.showAndWait();
+
+        Node source = (Node)event.getSource();
+        Stage stage = (Stage)source.getScene().getWindow();
+        stage.close();
+        con.databaseDisconnect();
+        }
+        else
+        {
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Tuxedo View");
+            alert.setContentText("Invalid name");
+            alert.showAndWait();
+        }
 
     }
     public User getUser() { return user; }
