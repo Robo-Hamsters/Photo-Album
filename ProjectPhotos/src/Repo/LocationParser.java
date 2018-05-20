b5931b3184060b602be9caa971a1afb5e6176ebe
Pavlina <pavlina.mitsou@gmail.com>
2018-05-20 23:56:22 +0300

    updated location parser

--- ProjectPhotos/src/Repo/LocationParser.java
+++ ProjectPhotos/src/Repo/LocationParser.java
@@ -1,42 +1,23 @@
 package Repo;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import org.json.*;
 
-public class LocationParser
-{
-    private JSONObject responce;
-    private String country = "";
-    private String city = "";
+
+public  class LocationParser {
+
+    private String country;
+    private String city;
     private double latitude;
     private double longitude;
+    private JSONObject responce;
 
-    public LocationParser(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-        retriveLocation();
-    }
-    public JSONObject getResponce() {
-        return responce;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-
-    public  void retriveLocation()
+    public void retriveLocation()
     {
         try {
 
@@ -54,15 +35,51 @@ public class LocationParser
                 responce = new JSONObject(inputLine);
                 JSONArray results = responce.getJSONArray("results");
 
-                if(results.length()>0)
+                for(int i = 0; i<results.length(); i++)
                 {
-                    country = results.getJSONObject(results.length()-1).getString("formatted_address");
-                    city = results.getJSONObject(3).getJSONArray("address_components").getJSONObject(0).getString("long_name");
+                    if(results.getJSONObject(i).getJSONArray("types").getString(0).equals("country"))
+                    {
+                        country = results.getJSONObject(i).getString("formatted_address");
+                    }
+
+                    if(results.getJSONObject(i).getJSONArray("types").getString(0).equals("locality"))
+                    {
+                        city = results.getJSONObject(i).getJSONArray("address_components").getJSONObject(0).getString("long_name");
+                    }
                 }
+
+                //country = results.getJSONObject(results.length()-1).getString("formatted_address");
+                //city = results.getJSONObject(3).getJSONArray("address_components").getJSONObject(0).getString("long_name");
             }
         } catch (FileNotFoundException e) {
-        } catch (IOException e) {
+        } catch (IOException | JSONException e) {
         }
-
     }
+
+    public String getCity() {
+        return city;
+    }
+
+    public LocationParser(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        retriveLocation();
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public JSONObject getResponce() {
+        return responce;
+    }
+
 }
